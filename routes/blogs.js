4bd5a91417a666834fb79d7b368923ff2a85c354
Casey Brannon <cbrannon412@gmail.com>
2019-05-30 09:44:33 -0700

    isloggedin

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,12 +2,17 @@ var router = require('express').Router()
 var blogsCtrl =  require('../controllers/blogs')
 
 
-router.get('/new', blogsCtrl.new);
+router.get('/new', isLoggedIn, blogsCtrl.new);
 router.get('/show', blogsCtrl.index);
 router.get('/blogs/:id',  blogsCtrl.viewPost);
 router.post('/blogs/:id/update', blogsCtrl.updatePost)
 router.post('/blogs/:id/delete', blogsCtrl.deletePost);
 router.post('/new', blogsCtrl.create);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+   }
 
 
 
